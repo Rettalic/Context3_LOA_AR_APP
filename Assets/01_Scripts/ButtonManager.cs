11a80b4e41f4645f40164da22d68733aae1d1cbc
Rettalic <ybroeks11@gmail.com>
2023-11-28 15:20:21 +0100

    Added Start of Dialogue system and added fonts
    Credits to "Diesel" for this Dialogue System.
    Added the Georgia font and the BebasNeue font.

---
+++ Assets/01_Scripts/ButtonManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour
+{
+    public GameObject startScreen;
+    public GameObject howTo;
+    public GameObject scanMaq;
+
+
+    public void EnableButton(GameObject _obj)
+    {
+
+    }
+}
