b72c72c7404bc5288411e17d949104a8a6cefc40
Rettalic <ybroeks11@gmail.com>
2023-11-30 10:14:25 +0100

    Made the UI buttons work for the test day

--- Assets/01_Scripts/TestUI.cs
+++ Assets/01_Scripts/TestUI.cs
@@ -1,18 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
 
 public class TestUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void SetObjectActive(GameObject openObj)
     {
-        
+        openObj.SetActive(true);
+    }
+    public void DisableObject(GameObject currentObj)
+    {
+        currentObj.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
